ccab25b077a872be5406c563ad88eb20675ba00c
itaidavid1 <itai.david1@mail.huji.ac.il>
2023-05-12 13:31:36 +0300

    with sort and map and shuffle

--- Exs/Ex3/MapReduceFramework.cpp
+++ Exs/Ex3/MapReduceFramework.cpp
@@ -3,6 +3,7 @@
 #include "pthread.h"
 #include "Barrier.h"
 #include <algorithm>
+#include <map>
 
 ///////
 
@@ -17,8 +18,11 @@ struct JobContext {
     pthread_t * threads;
     JobState  jobState;
     std::atomic<int> atomic_counter;
-    ThreadContext t_context;
+    std::atomic<int> shuffle_vector_size;
+    std::atomic<int> reduce_counter;
+    ThreadContext* t_context;
     InputVec* inputVec;
+    IntermediateVec* shuffle_vector;
     OutputVec * outputVec;
     MapReduceClient * client;
     pthread_mutex_t map_mutex;
@@ -47,7 +51,7 @@ void start_map_stage(JobContext* jobContext){
         jobContext->jobState.stage = MAP_STAGE;
 
     }
-    //counter++
+    //TODO: create the weird vector with the bit shifting
 
     // mutex unlock
     if(pthread_mutex_unlock(&jobContext->map_mutex)!=0){
@@ -82,7 +86,40 @@ void do_sort(ThreadContext* threadContext){
     std::sort(threadContext->intermediate_vector->begin(),threadContext->intermediate_vector->end())
 }
 
+void do_shuffle(ThreadContext* threadContext){
+    struct CompareKeys {
+        bool operator()(const K2* first, const K2* second) const {
+            return *first<*second;
+        }
+    };
+    IntermediateVec* shuffle_vec = threadContext->jobContext->shuffle_vector;
+    ThreadContext* allThreadContext = threadContext->jobContext->t_context;
+    std::map<K2*,IntermediateVec,CompareKeys> shuffle_map;
 
+    for (ThreadContext* cur_t_context : allThreadContext){
+        IntermediateVec temp_vec = cur_t_context->intermediate_vector;
+        for (IntermediatePair K_V_pair : temp_vec){
+            if (shuffle_map.find(K_V_pair.first) == shuffle_map.end()){
+                shuffle_map[K_V_pair.first] = new IntermediateVec();
+                shuffle_map.at(K_V_pair.first).push_back(K_V_pair);
+            }
+            else{
+                shuffle_map.at(K_V_pair.first).push_back(K_V_pair);
+            }
+        //TODO : add atomic counter
+        }
+    }
+    for (auto key_to_vals :shuffle_map){
+        shuffle_vec->push_back(key_to_vals.second);
+    }
+
+}
+
+void do_reduce(ThreadContext* threadContext){
+
+
+
+}
 
 void * run_map_reduce(void* arg){
     ThreadContext* t_context = (ThreadContext*) arg;
@@ -96,9 +133,13 @@ void * run_map_reduce(void* arg){
     t_context->jobContext->barrier->barrier();
 
     // do shuffle
+    if (t_context->tid == 0){
+        do_shuffle(t_context);
+    }
 
-
+    t_context->jobContext->barrier->barrier();
     // do reduce
+    do_reduce(t_context);
 
 
 }
@@ -111,6 +152,7 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     .t_context = new ThreadContext[multiThreadLevel],
     .threads  = new pthread_t[multiThreadLevel],
     .inputVec = inputVec,
+    .shuffle_vector = new IntermediateVec(),
     .outputVec = outputVec,
     .client = client,
     .jobState = new JobState {UNDEFINED_STAGE, 0.0},
